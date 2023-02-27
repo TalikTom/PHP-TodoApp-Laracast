3b2e7431b462aaf24dc3e429d6d7d0da50f8af5a
Luka Agic <agic.luke@gmail.com>
2023-02-28 00:46:16 +0100

    Refactor status code conditions into methods

--- controllers/note.php
+++ controllers/note.php
@@ -6,20 +6,14 @@ $db = new Database($config['database']);
 $heading = 'Note';
 
 $note = $db->query('select * from notes where id = :id', [
-      'id' => $_GET['id']])->fetch();
+      'id' => $_GET['id']])->findOrFail();
 
 
-// if there is no note under specific id number show 404(default for abort)
-if (! $note) {
-    abort(Response::NOT_FOUND);
-}
-
-
-// currently hard coding user, show 403 if user not matching
 $currentUserId = 1;
 
-if ($note['user_id'] !== $currentUserId) {
-    abort(Response::FORBIDDEN);
-}
+authorize($note['user_id'] === $currentUserId);
+
+
+
 
 require "views/note.view.php";
