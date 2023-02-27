867632e65ebc101444c932724e5924327dd51fcc
Luka Agic <agic.luke@gmail.com>
2023-02-28 00:11:42 +0100

    Add 403 page, practice authorization

--- controllers/note.php
+++ controllers/note.php
@@ -5,6 +5,21 @@ $db = new Database($config['database']);
 
 $heading = 'Note';
 
-$note = $db->query('select * from notes where id = :id', ['id' => $_GET['id']])->fetch();
+$note = $db->query('select * from notes where id = :id', [
+      'id' => $_GET['id']])->fetch();
+
+
+// if there is no note under specific id number show 404(default for abort)
+if (! $note) {
+    abort(Response::NOT_FOUND);
+}
+
+
+// currently hard coding user, show 403 if user not matching
+$currentUserId = 1;
+
+if ($note['user_id'] !== $currentUserId) {
+    abort(Response::FORBIDDEN);
+}
 
 require "views/note.view.php";
