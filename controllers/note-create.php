b7cfd856a1131331aed23e4d10bde23a17b91ea0
Luka Agic <agic.luke@gmail.com>
2023-02-28 23:30:21 +0100

    Escape untrusted input, validate for empty input

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -6,6 +6,15 @@ $db = new Database($config['database']);
 $heading = 'Create note';
 
 if($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    //add errors array
+    $errors = [];
+
+    //check if textarea is empty, if it is empty push the error message into $errors array with key being 'body'
+    if(strlen($_POST['body']) === 0) {
+        $errors['body'] = 'A body of text is required';
+    }
+
     $db->query('INSERT INTO notes(body, user_id) VALUES(:body, :user_id)', [
         'body' => $_POST['body'],
         'user_id' => 1,
